82ed487b04643f1ee409aeaf8dfde8b5da3da551
ravchou <47667020+ravchou@users.noreply.github.com>
2019-02-28 00:10:01 +0100

    Mytaxi home test

--- app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/ExampleInstrumentedTest.java
@@ -1,68 +1,12 @@
 package com.mytaxi.android_demo;
 
-import android.app.Activity;
-import android.app.Instrumentation;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.ContactsContract;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.IdlingPolicies;
-import android.support.test.espresso.ViewAssertion;
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.assertion.ViewAssertions;
-import android.support.test.espresso.matcher.RootMatchers;
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.widget.AdapterView;
 
-import com.mytaxi.android_demo.activities.MainActivity;
-
-import org.hamcrest.Matchers;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import static android.support.test.espresso.Espresso.onData;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.pressBackUnconditionally;
-import static android.support.test.espresso.action.ViewActions.pressKey;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
-import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
-import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
-import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
-import static android.support.test.espresso.matcher.ViewMatchers.withSubstring;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.v4.content.ContextCompat.startActivity;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.equalToIgnoringCase;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.hamcrest.core.IsNot.not;
-import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.*;
 
 /**
@@ -72,64 +16,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
-    public ExampleInstrumentedTest() {
-        IdlingPolicies.setMasterPolicyTimeout(3000, TimeUnit.SECONDS);
-    }
-
-    @Before
-    public void init(){
-
-    }
     @Test
-    public void Login() throws InterruptedException , IOException{
-        onView(withId(R.id.edt_username)).perform(typeText("crazydog335"));
-        onView(withId(R.id.edt_password)).perform(typeText("venture"));
-        onView(withId(R.id.btn_login)).perform(click());
-        Thread.sleep(5000);
-        onView(withId(R.id.textSearch)).check(matches(isDisplayed()));;
-    }
-
-    @Test
-
     public void useAppContext() throws Exception {
-        //onView(withId(R.id.edt_username)).perform(typeText("crazydog335"));
-        //onView(withId(R.id.edt_password)).perform(typeText("venture"));
-        //onView(withId(R.id.btn_login)).perform(click());
-        Thread.sleep(5000);
-        onView(withId(R.id.textSearch)).perform(typeText("sa"));
-        Thread.sleep(3000);
-        onView(withText("Sarah Scott")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
-        Thread.sleep(2000);
-        onView(withId(R.id.fab)).perform(click());
-        Thread.sleep(5000);
-        onView(withContentDescription("dial")).perform(click());
-        Thread.sleep(5000);
-        //onView(withContentDescription("End")).perform(click());
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
 
-
-
+        assertEquals("com.mytaxi.android_demo", appContext.getPackageName());
     }
-        //onData(equalTo("Sarah Scott")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
-        //onData(instanceOf("Sarah Scott".class)).inRoot(RootMatchers.withDecorView(not(is(mActivityRule.getActivity().getWindow().getDecorView())))).perform(ViewActions.click());
-        //onView(allOf(withId(R.id.textSearch), withSubstring("Sarah"))).check(matches(isDisplayed()));
-
-        //onData(allOf(is(instanceOf(String.class)), is("Sarah Scott"))).perform(click());
-
-        //onView(withText("Sarah Scott")).check(matches(isDisplayed())).perform(click());
-
-        //onData(hasItem("Sarah Scott")).perform(click());
-        //onData(anything()).inAdapterView(withClassName(equalTo("android.widget.EditText")).atPosition(1).perform(click()));
-        //onData(anything()).inAdapterView(isDescendantOfA(withId(R.id.textSearch))).atPosition(2).perform(click());
-        //assertEquals("com.mytaxi.android_demo", appContext.getPackageName());
-
-
-
-     @After
-  public  void Teardown() throws Exception{
-       onView(withContentDescription("Open navigation drawer")).perform(click());
-    onView(withId(R.id.design_menu_item_text)).perform(click());
-}
 }
--- app/src/androidTest/java/com/mytaxi/android_demo/MyTaxi.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/MyTaxi.java
@@ -1,30 +1,29 @@
 package com.mytaxi.android_demo;
 
-import android.support.test.espresso.IdlingPolicies;
 import android.support.test.espresso.matcher.RootMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
 import com.mytaxi.android_demo.activities.MainActivity;
-
 import org.junit.After;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ * The test will login to My taxi App and call the driver with name like sa
+ */
 
 @RunWith(AndroidJUnit4.class)
 public class MyTaxi {
@@ -32,20 +31,19 @@ public class MyTaxi {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
+        //The Method will login to the my taxi app on the anroid device with the given user name and password
         @Before
-        public void Login()throws InterruptedException , IOException{
+        public void init(){
+
+        }
+
+       //The test will login to mytaxi app and call the taxi with the name like 'sa'
+        @Test
+        public void callTaxi() throws Exception {
             onView(withId(R.id.edt_username)).check(matches(isDisplayed())).perform(typeText("crazydog335"));
             onView(withId(R.id.edt_password)).check(matches(isDisplayed())).perform(typeText("venture"));
             onView(withId(R.id.btn_login)).check(matches(isDisplayed())).perform(click());
             Thread.sleep(3000);
-            onView(withId(R.id.textSearch)).check(matches(isDisplayed()));;
-        }
-
-        @Test
-        public void callTaxi() throws Exception {
-            //onView(withId(R.id.edt_username)).perform(typeText("crazydog335"));
-            //onView(withId(R.id.edt_password)).perform(typeText("venture"));
-            //onView(withId(R.id.btn_login)).perform(click());
             onView(withId(R.id.textSearch)).check(matches(isDisplayed())).perform(typeText("sa"));
             Thread.sleep(3000);
             onView(withText("Sarah Scott")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
@@ -57,9 +55,10 @@ public class MyTaxi {
             //onView(withContentDescription("End")).perform(click());
         }
 
+        //As part of the excepion or after every successfull call the account will be logged off
         @After
         public  void Teardown() throws Exception{
-           // onView(withContentDescription("Open navigation drawer")).check(matches(isDisplayed())).perform(click());
-            //onView(withId(R.id.design_menu_item_text)).check(matches(isDisplayed())).perform(click());
+           onView(withContentDescription("Open navigation drawer")).check(matches(isDisplayed())).perform(click());
+           onView(withId(R.id.design_menu_item_text)).check(matches(isDisplayed())).perform(click());
         }
 }
